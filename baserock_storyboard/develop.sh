e71a84501df5b25933a69ba4ad798b6285060958
Pedro Alvarez <pedro.alvarez@codethink.co.uk>
2015-12-18 11:56:10 +0000

    baserock_storyboard: Stop using puppet / packer for deployment
    Change-Id: I04be3327312176282aa29cda47253a510d8dc43b

--- baserock_storyboard/develop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Set up a development environment in a container.
-
-exec docker run -i -t --rm \
-    --publish=127.0.0.1:80:80 \
-    --volume=`pwd`:/src/test-baserock-infrastructure \
-    baserock/storyboard
-
